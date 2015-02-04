db83d071b8185427fd5786efd3ff25efbd86052f
Piotr B <b.piotr@gmail.com>
2015-02-04 01:06:46 +0100

    Added rating listing

--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -2,15 +2,23 @@ require 'test_helper'
 
 class ListingBooksTest < ActionDispatch::IntegrationTest
   setup do
-    Book.create!(title: 'Pragmatic Programmer', rating: 5)
+    Book.create!(title: 'Pragmatic Programmer', rating: 4)
     Book.create!(title: "Ender's Game", rating: 5)
   end
 
   test 'listing books' do
-     get '/books'
+    get '/books'
 
-     assert_equal 200, response.status
-     assert_equal Mime::JSON, response.content_type
-     assert_equal Book.count, JSON.parse(response.body).size
-   end
+    assert_equal 200, response.status
+    assert_equal Mime::JSON, response.content_type
+    assert_equal Book.count, JSON.parse(response.body).size
+  end
+
+  test 'list top rated books' do
+    get '/books?rating=5'
+
+    assert_equal 200, response.status
+    assert_equal Mime::JSON, response.content_type
+    assert_equal 1, JSON.parse(response.body).size
+  end
 end
